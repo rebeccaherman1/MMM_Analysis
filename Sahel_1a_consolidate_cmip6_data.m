df93ceafcf0a0522c7b2f10ad42580862b3d181f
Rebecca Jean Herman <rebeccaherman1@berkeley.edu>
2020-08-05 04:34:33 -0400

    moved time mask to Sahel_1a. Re-ran.

--- Sahel_1a_consolidate_cmip6_data.m
+++ Sahel_1a_consolidate_cmip6_data.m
@@ -3,15 +3,18 @@ model_file_name = 'data/cmip6_h_all.mat';
 folder = '~/netcdf/cmip6/historical';
 files = split(ls(folder));
 next_line = 1;
+T = 1850:1:2014;
 for file = files(1:end-1)'
     names = make_model_and_p_name(file);
     model(next_line, 1:3) = names;
     fopen_name = [folder, '/', file{:}];
-    T = ncread(fopen_name, 'year');
+    Time = ncread(fopen_name, 'year');
     l = length(T);
-    if T(1)<=1901 && T(end)>=2003
-        runs(next_line,1:l)   = ncread(fopen_name, 'pr');
-        time(next_line,1:l)   = T;
+    if Time(1)<=1901 && Time(end)>=2003
+	T_x = ismember(Time, T);
+	pr = ncread(fopen_name, 'pr');
+        runs(next_line,1:l)   = pr(T_x); 
+        time(next_line,1:l)   = T(T_x);
         next_line=next_line+1;
         save(model_file_name,'model','runs', 'time');
     end
