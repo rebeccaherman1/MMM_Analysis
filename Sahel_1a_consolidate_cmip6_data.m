e9e64f16a9c92999ce11a95f421586a501ee7e88
Rebecca Jean Herman <rebeccaherman1@berkeley.edu>
2020-08-14 01:20:10 -0400

    put Sahel_1a in a for loop. Downloaded new data from the cloud, and re-compiled everything.

--- Sahel_1a_consolidate_cmip6_data.m
+++ Sahel_1a_consolidate_cmip6_data.m
@@ -1,39 +1,44 @@
 clear
 variable = 'ts';
-model_file_name = ['data/cmip6_', variable, '_piC_all.mat'];
-folder = '~/netcdf/cmip6/preprocessed/piControl';
-files = split(ls(folder));
-files = files(contains(files, [variable, '_']));
-next_line = 1;
-T = 1850:1:2014;
-for file = files'
-    names = make_model_and_p_name(file);
-    model(next_line, 1:3) = names;
-    fopen_name = [folder, '/', file{:}];
-    %ncdisp(fopen_name)
-    INFO = ncinfo(fopen_name);
-    if any(contains({INFO.Variables.Name}, {'time'}))
-    	Time = ncread(fopen_name, 'time');
-    else
-	Time = ncread(fopen_name, 'year');
-    end
-    %pr = ncread(fopen_name, 'pr');
-    NA = ncread(fopen_name, 'NA');
-    GT = ncread(fopen_name, 'GT');
-    NARI = ncread(fopen_name, 'NARI');
-    pr = cat(3, NA, GT, NARI);
-    if contains(model_file_name, 'piC')
-	l = length(Time);
-    elseif Time(1)<=1901 && Time(end)>=2014
-	T_x = (Time >= 1901) & (Time <= 2014);
-	Time = Time(T_x);
-	pr = pr(T_x,:,:);
-	l = length(pr);
-	if(length(Time)~=length(pr))
-	    fprintf('pr contains fill values');
+
+scenarios = {'historical', 'hist-aer', 'hist-nat', 'hist-GHG', 'piControl'};
+short_names = {'h', 'a', 'n', 'g', 'piC'};
+for i = 1:length(scenarios)
+    model_file_name = ['data/cmip6_', variable, '_', short_names{i}, '_all.mat']
+    folder = ['~/netcdf/cmip6/preprocessed/', scenarios{i}];
+    files = split(ls(folder));
+    files = files(contains(files, [variable, '_']));
+    next_line = 1;
+    T = 1850:1:2014;
+    for file = files'
+        names = make_model_and_p_name(file);
+        model(next_line, 1:3) = names;
+        fopen_name = [folder, '/', file{:}];
+        %ncdisp(fopen_name)
+        INFO = ncinfo(fopen_name);
+        if any(contains({INFO.Variables.Name}, {'time'}))
+        	Time = ncread(fopen_name, 'time');
+        else
+            Time = ncread(fopen_name, 'year');
         end
-    else
-	continue
+        %pr = ncread(fopen_name, 'pr');
+        NA = ncread(fopen_name, 'NA');
+        GT = ncread(fopen_name, 'GT');
+        NARI = ncread(fopen_name, 'NARI');
+        pr = cat(3, NA, GT, NARI);
+        if contains(model_file_name, 'piC')
+            l = length(Time);
+        elseif Time(1)<=1901 && Time(end)>=2014
+            T_x = (Time >= 1901) & (Time <= 2014);
+            Time = Time(T_x);
+            pr = pr(T_x,:,:);
+            l = length(pr);
+            if(length(Time)~=length(pr))
+                fprintf('pr contains fill values');
+            end
+        else
+            continue
+        end
     end
     runs(next_line,1:l,1:3)   = pr; 
     time(next_line,1:l)   = Time;
