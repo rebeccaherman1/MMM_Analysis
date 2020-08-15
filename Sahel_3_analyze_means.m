30a7f9fa9a880bcdaa80e3e313497d8031a4a2a7
rebeccaherman1@berkeley.edu <rebeccaherman1@berkeley.edu>
2020-08-14 19:32:00 -0700

    modified Sahel_3 to accept ts with 3 indices. However, some simulations contain NaN from the original consolidated data files. I'll need to investigate that, and either fix the problem or remove those simulations.

--- Sahel_3_analyze_means.m
+++ Sahel_3_analyze_means.m
@@ -1,53 +1,43 @@
 clear
 N = 500;
-short=false;
 
 dt = "";%, "detrended"];
 %fl = "last";%, "first"];
 scenarios = {'cmip6_h', 'cmip6_a', 'cmip6_n', 'cmip6_g'};%v'};%'r'};%'a6'};%'e'};%'h'};%,'a','n','g'};%'amip',; 
+variable = 'ts';
 
-global start_year; 
-if(short)
-    start_year = 1950;% 
-else
-    start_year = 1901;%
-end
-global end_year; end_year = 2003;
-global ref_T_years ref_T_amip
+global start_year end_year ref_T_years
+start_year = 1901;
+end_year = 2014; 
 
 mkdir('analysis')
+mkdir(['analysis/', variable])
 
-obs = load('data/historical_precipitation.mat');
-obs_anomaly = obs.prcp-mean(obs.prcp);
-ref_T_years = obs.T; ref_T_amip = start_year:end_year;
+obs = load(['data/', variable, '/observations.mat']);
+obs_anomaly = obs.var-mean(obs.var);
+timeframe_obs = (obs.T >= start_year & obs.T <= end_year);
+ref_T_years = obs.T(timeframe_obs);
+o = obs_anomaly(:,timeframe_obs,:);
 
 for j = 1:length(scenarios)
     scenario = scenarios{j};
     fprintf("Accessing scenario %s\n", scenario);
 
-    h = load(['data/', scenario,'_GM.mat']);
+    h = load(['data/', variable, '/', scenario,'_GM.mat']);
+    fname = ['analysis/', variable, '/', scenario, '_', num2str(ref_T_years(1)), '-', num2str(ref_T_years(end)), '_N', num2str(N)];
+    hall = load(['data/', variable, '/', scenario, '_all.mat']);
     num_models = length(h.models); %num_pC_models = length(h.piC_models); 
-    fname = ['analysis/', scenario, '_N', num2str(N)];
-    if(short)
-        Analysis = matfile([fname, '_', num2str(start_year)],'Writable', true);%'_short'], 
-    else
-        Analysis = matfile(fname, 'Writable', true);
-    end
+    Analysis = matfile(fname, 'Writable', true);
 
-    T_obs = ref_T_years>=start_year & ref_T_years<=end_year;
-    if(strcmp(scenario, 'amip')==2 || short)
-        T_m = ref_T_amip>=start_year & ref_T_amip<=end_year;
-    else
-        T_m = T_obs;
-    end
-    hm = h.GMs(:,T_m); %TODO should actually add a T record to the data I save :/
-    trust = h.trust; o = obs_anomaly(T_obs); 
+    T_m = ismember(single(h.time(1,:)),ref_T_years);
+    hm = h.GMs(:,T_m, :); %TODO should actually add a T record to the data I save :/
+    trust = h.trust;
 
     [r, e, mmm] = calc_stats(hm, trust, o);
     MMM.r = r; MMM.e = e; MMM.MMM = mmm; Analysis.MMM = mmm;
     
-    hall = load(['data/', scenario, '_all.mat']);
-    runs = hall.runs(:,T_m);
+    %hall
+    runs = hall.runs(:,T_m,:);
     runs_r = m_corrcoef(runs, o); runs_e = rmse(runs, o);
     indiv_runs.r = runs_r; indiv_runs.e = runs_e; indiv.models = hall.model;
     
@@ -62,13 +52,15 @@ for j = 1:length(scenarios)
     
     Analysis.MMM = MMM; Analysis.indiv = indiv; Analysis.sanity = sanity; Analysis.N = N; Analysis.indiv_runs = indiv_runs;
       
-    Analysis.historical_bootstrapped = bootstrap_model(N, obs_anomaly, T_obs, hm, trust);
+    Analysis.historical_bootstrapped = bootstrap_model(N, o, timeframe_obs, hm, trust);
     
     %comment out for AMIP simulations.
-    [Analysis.piC_resampled_bootstrapped, skip_models] = sample_model(N, obs_anomaly, T_obs, h.piC_GMs, h.piC_trust, dt);
+    [Analysis.piC_resampled_bootstrapped, skip_models] = sample_model(N, o, timeframe_obs, h.piC_GMs, h.piC_trust, dt);
     fprintf('skipping piC simulations which are too short:')
     h.piC_models(skip_models,:)
 end
+
+%% functions
 
 %T can be a logical array or a range.
 %TODO put detrending in here
@@ -77,26 +69,34 @@ function [r, e, mmm] = calc_stats(means, trust, obs)
     %mmm_std = mean(std(means,0,2));
     mmm = mean(weights.*means,1);
     e = rmse(mmm, obs);
-    R = corrcoef(mmm, obs);
-    r = R(1,2);
-    if(isnan(r))
-        r = 0;
+    [~,~,s3]=size(means);
+    r=nan([1,1,s3]);
+    for i=1:s3
+        R = corrcoef(mmm(:,:,i), obs(:,:,i));
+        R = R(1,2);
+        if(isnan(r))
+            R = 0;
+        end
+        r(i)=R;
     end
 end
 
 function [all] = bootstrap_model(N, obs, T, GMs, trust, fl, dt)
-    global start_year end_year
+    run_length = sum(T);
     num_models=length(trust);
+    [s1,s2,s3]=size(GMs);
     if(nargin<6)
         means = GMs;%(:,T); TODO
     elseif(strcmp(fl, "first"))
-        means = GMs(:,1:(end_year-start_year+1));
+        means = GMs(:,1:(end_year-start_year+1),:);
     elseif(strcmp(fl, "last"))
-        length_of_GMs = sum(~isnan(a),2);
-        idx_raw = cumsum(ones(size(GMs)), 2);
-        idx = (idx_raw > length_of_GMs - sum(T)) & (idx_raw <= length_of_GMs);
+        length_of_GMs = sum(~isnan(GMs(:,:,1)),2);
+        idx_raw = cumsum(ones([s1,s2]), 2);
+        idx = (idx_raw > length_of_GMs - run_length) & (idx_raw <= length_of_GMs);
         m_t = mod.means'; idx_t = idx';
-        means = vec2mat(m_t(idx_t), sum(T));
+        %technically I should put this in a for loop, but I don't really
+        %care!!!!! because I don't use first and last anymore.
+        means = vec2mat(m_t(idx_t), run_length);
     else
         fprintf("unrecognized option for 'fl': %s", fl);
         rs = NA; es = NA; 
@@ -104,18 +104,18 @@ function [all] = bootstrap_model(N, obs, T, GMs, trust, fl, dt)
     end
     means = means - mean(means, 2);
     if(nargin>6 && strcmp(dt, "detrended"))
-        means = detrend(means')';
+        means = permute(detrend(permute(means,[2,1,3])), [2,1,3]);
     end
 
-    rs = zeros(N,1); es = zeros(N,1);
-    mmms = zeros(N, end_year-start_year+1);
+    rs = zeros(N,1,s3); es = zeros(N,1,s3);
+    mmms = zeros(N, run_length,s3);
     for bs = 1:N
         index=randi(num_models,num_models,1);
-        reordered_means=means(index,:);
+        reordered_means=means(index,:,:);
         reordered_trust=trust(index);
-        [r, e, mmm] = calc_stats(reordered_means, reordered_trust, obs(T));
-        mmms(bs,:) = mmm; 
-        rs(bs) = r; es(bs) = e;
+        [r, e, mmm] = calc_stats(reordered_means, reordered_trust, obs(:,T,:));
+        mmms(bs,:,:) = mmm; 
+        rs(bs,:,:) = r; es(bs,:,:) = e;
     end
     [CI_low, CI_high] = confidence_interval(mmms);
     b_means = mmms; mids = mean(mmms, 1);
@@ -126,17 +126,18 @@ end
 
 function [all, too_short] = sample_model(N, obs, T, GMs, trust, dt, fl)
     length_run = sum(T);
-    length_of_GMs = sum(~isnan(GMs),2);
+    length_of_GMs = sum(~isnan(GMs(:,:,1)),2);
     too_short = length_of_GMs<length_run;
     
     length_of_GMs = length_of_GMs(~too_short);
-    GMs = GMs(~too_short,:); trust = trust(~too_short);
-    idx_raw = cumsum(ones(size(GMs)), 2);
+    GMs = GMs(~too_short,:,:); trust = trust(~too_short);
+    [s1,s2,s3] = size(GMs);
+    idx_raw = cumsum(ones([s1,s2]), 2);
     
     num_models=length(trust);
 
-    rs = zeros(N,1); es = zeros(N,1); 
-    mmms = zeros(N, length_run);
+    rs = zeros(N,1,s3); es = zeros(N,1,s3); 
+    mmms = zeros(N, length_run,s3);
     for bs = 1:N
         if (nargin>6 && strcmp(fl, "last"))
             rand_end_offset = length_of_GMs;
@@ -147,20 +148,21 @@ function [all, too_short] = sample_model(N, obs, T, GMs, trust, dt, fl)
             end    
         end
         idx = (idx_raw > rand_end_offset - length_run) & (idx_raw <= rand_end_offset);
-        m_t = GMs'; idx_t = idx';
-        means = vec2mat(m_t(idx_t), length_run);
+        m_t = permute(GMs, [2,1,3]); idx_t = repmat(idx',1,1,s3);
+        means = permute(reshape(m_t(idx_t), length_run,s1,s3), [2,1,3]);
         means = means - mean(means, 2);
         
         if(nargin>5 && strcmp(dt, "detrended"))
+            %this won't work with SSTs!
             means = detrend(means')';
         end
         %still bootstrap after
         index=randi(num_models,num_models,1);
-        means=means(index,:);
+        means=means(index,:,:);
 
-        [r, e, mmm] = calc_stats(means, trust(index), obs(T));
-        mmms(bs,:) = mmm;
-        rs(bs) = r; es(bs) = e; 
+        [r, e, mmm] = calc_stats(means, trust(index), obs(:,T,:));
+        mmms(bs,:,:) = mmm;
+        rs(bs,:,:) = r; es(bs,:,:) = e; 
     end
     [CI_low, CI_high] = confidence_interval(mmms); mids = mean(mmms, 1);
     r_means = mmms;
@@ -173,14 +175,14 @@ end
 %DATA_M with a single time series in a horizontal vector data_v. Returns a
 %vertical vector of Correlation Coefficients.
 function [cc] = m_corrcoef(data_m, data_v)
-    n = length(data_v);
-    mu_m = mean(data_m, 2); mu_v = mean(data_v);
+    [~,n,~] = size(data_v);
+    mu_m = mean(data_m, 2); mu_v = mean(data_v,2);
     %"1" signifies that we will use N instead of N-1.
-    sigma2_m = var(data_m,1,2); sigma2_v = var(data_v,1);
-    cc = 1/n*(data_m-mu_m)*(data_v'-mu_v)./(sigma2_m.*sigma2_v).^(.5);
+    sigma2_m = var(data_m,1,2); sigma2_v = var(data_v,1,2);
+    cc = 1/n*sum((data_m-mu_m).*(data_v-mu_v),2)./(sigma2_m.*sigma2_v).^(.5);
 end
 
 function [error] = rmse(data_m, data_v)
-    data_m = data_m-mean(data_m, 2); data_v = data_v - mean(data_v);
-    error = mean((data_m - data_v).^2,2).^(.5)/std(data_v,1);
+    data_m = data_m-mean(data_m, 2); data_v = data_v - mean(data_v,2);
+    error = mean((data_m - data_v).^2,2).^(.5)./std(data_v,1,2);
 end    
