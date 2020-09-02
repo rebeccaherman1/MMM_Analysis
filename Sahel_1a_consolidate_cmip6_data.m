01d8724095719617c08a2661ff127ab2be5c8fcb
rebeccaherman1@berkeley.edu <rebeccaherman1@berkeley.edu>
2020-09-01 22:34:49 -0700

    added description: only used for preprocessed files from the cloud via jupyter

--- Sahel_1a_consolidate_cmip6_data.m
+++ Sahel_1a_consolidate_cmip6_data.m
@@ -1,8 +1,13 @@
+%Only use on PREPROCESSED data which was downloaded from the cloud via my
+%jupyter script. For data I downloaded in full, see matlab code
+%Sahel_1_save_data_amip.
+
 clear
 variable = 'ts';
 
 scenarios = {'historical', 'hist-aer', 'hist-nat', 'hist-GHG', 'piControl'};
 short_names = {'h', 'a', 'n', 'g', 'piC'};
+
 for i = 1:length(scenarios)
     clear model runs time
     model_file_name = ['data/', variable, '/cmip6_', short_names{i}, '_all.mat']
@@ -10,7 +15,6 @@ for i = 1:length(scenarios)
     files = split(ls(folder));
     files = files(contains(files, [variable, '_']));
     next_line = 1;
-    T = 1850:1:2014;
     for file = files'
         names = make_model_and_p_name(file);
         model(next_line, 1:3) = names;
@@ -19,7 +23,7 @@ for i = 1:length(scenarios)
         INFO = ncinfo(fopen_name);
         if any(contains({INFO.Variables.Name}, {'time'}))
             Time = ncread(fopen_name, 'time');
-        else
+        elseif any(contains({INFO.Variables.Name}, {'year'}))
             Time = ncread(fopen_name, 'year');
         end
 	if(strcmp(variable, 'pr'))
@@ -60,10 +64,10 @@ end
 function [model_names] = make_model_and_p_name(file)
 % special version for PSL-FACTS
     fields = split(file, [".", "_"]);
-    institution = fields{2};
     model = fields{3};
     run = fields{4};
     run_stats = split(run, ["p", "f"]);
     model_and_p = [model, ' p', run_stats{2}];
+    institution = fields{2};
     model_names = {institution, model_and_p, run};
 end
