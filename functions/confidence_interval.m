8c723a13cb2ebca3e72a5ebda9edd340625d8b0f
rebeccaherman1@berkeley.edu <rebeccaherman1@berkeley.edu>
2020-09-03 15:16:50 -0700

    updated confidence_interval to take a dimension

--- functions/confidence_interval.m
+++ functions/confidence_interval.m
@@ -1,7 +1,17 @@
-function [CI_low, CI_high] = confidence_interval(X, confidence) %goes by columns
-if(nargin < 2)
-    confidence = .025;
-end
-X = sort(X, 1); s= size(X); n = s(1); side = round(n * confidence);
-CI_low = X(side+1,:,:); CI_high = X(n - side,:,:);
+function [CI_low, CI_high] = confidence_interval(X, dim, confidence) %goes by columns
+    if(nargin < 2 || isempty(dim))
+        dim = 1;
+    end
+    if(nargin < 3 || isempty(confidence))
+        confidence = .025;
+    end
+    X = sort(X, dim); n = size(X, dim); side = round(n * confidence);
+    switch dim
+        case 1
+            CI_low = X(side+1,:,:); CI_high = X(n - side,:,:);
+        case 2
+            CI_low = X(:,side+1,:); CI_high = X(:,n - side,:);
+        otherwise
+            fprintf('bad dimension!')
+    end
 end
