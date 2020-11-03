c1c6f8b75bb0b613e7f5986ebfb9361b4e10d00c
rebeccaherman1@berkeley.edu <rebeccaherman1@berkeley.edu>
2020-11-03 10:09:43 -0500

    separated downloading OBS from simulations. Cleaned up obs code. Removed time constraints. TODO: make work for TS, and remove time constraints from simulations.

--- Sahel_1_save_data_ingrid.m
+++ Sahel_1_save_data_ingrid.m
@@ -1,14 +1,10 @@
 %{
 Use for all data accessed without jupyter and the cloud, rather through ingrid. 
 Includes CMIP5 and Vanilla AMIP simulations, as well as certain AMIP+RAD
-simulations.
-
-Downloads data for all models and runs used in Herman et al. 2020 
-and in its followup paper (in progress), saving them in a single file.
+simulations, and observations.
 
 Log files for seach scenario "<Scenario>_log.txt" detail which simulations
 were skipped and why.
-TODO automate the log and the models.mat creation
 
 %CMIP6 institutions file calculated using the following code, after 
 downloading CMIP6 historical simulations using jupyter:
@@ -18,11 +14,12 @@ institutions = H.model(ia, 1:2);
 save('data/institutions_cmip6.mat', 'institutions');
 %}
 
-%TODO: update the file naming functions in observations
-%TODO: just save all available years?
+%TODO doesn't work for ts yet...
+%TODO: remove checking the years? or change it a little?
 
 clear
-get_observations = false;
+get_observations = true;
+get_historical = false;
 
 %Create MONTH_NAMES and MONTH_DAYS matrices for use in changing units later
 month_names = {'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'};
@@ -39,83 +36,100 @@ shortcuts = {'amip-piF'};%'p'};%'h', 'a', 'n', 'g', 'piC'};%, 'a6'};
 Generation = 6;
 variable = 'pr';
 
-start_month = [7];
-end_month = [9];
+start_month = 7;
+end_month = 9;
+month0 = month_names{start_month};
+month1 = month_names{end_month};
+%TODO this is only true if models don't just use 30-day months....
+days_per_month = mean(month_days(start_month:end_month));
+
 start_year = 1901;
 end_year = 2014;
 
 global ref_T_years
 ref_T_years = (start_year:1:end_year)';
 
-s_lat = 12;
-n_lat = 18;
+switch variable
+    case 'pr'
+        s_lat = 12;
+        n_lat = 18;
+        e_lon = -20;
+        w_lon = 40;
+    case 'ts'
+        %TODO fill in for TS!!!
+end
+        
 
 umbrella=load(['data/institutions_cmip', num2str(Generation), '.mat']);
 
 space_range_average = [... also includes some background time stuff...
-    '/.pr/',...   
-    'time//T/renameGRID/',... rename grid if needed.
-    'T/1/monthlyAverage/',... changes units from months to years
-    'lon/-20/40/RANGE/',... zonal range
-    'lat/', num2str(s_lat), '/', num2str(n_lat), '/RANGE',... meridional range
+    '/.pr',...   
+    '/time//T/renameGRID',... rename grid if needed.
+    '/T/1/monthlyAverage',... changes units from months to years
+    '/lon/', num2str(e_lon), '/', num2str(w_lon), '/RANGE',... zonal range
+    '/lat/', num2str(s_lat), '/', num2str(n_lat), '/RANGE',... meridional range
     '%7B/lat/cosd/%7D/',... box size changes with lat
     '%5Blon/lat%5D/weighted-average/']; % zonal and meridional average
 
-for i = 1:length(start_month)
-    month0 = month_names{start_month(i)};
-    month1 = month_names{end_month(i)};
-    mkdir(['data/', month0, '-', month1])
+%time range currently unused. 
+time_range = ['T/%28', month0, '%20', num2str(start_year), '%29%28',...
+    month1, '%20', num2str(end_year), '%29RANGE/'];
+
+seasonal_average = ['T/%28', month0, '-', month1,'%29/seasonalAverage/',...
+    'T/12/STEP/dods'];
+url_end_h = [space_range_average, seasonal_average]; %I removed the time_range!
+url_end_piC = [space_range_average, seasonal_average];
+
+%Observations
+if(get_observations)
+    ObsFile = matfile(['data/', variable, '/observations.mat'],'Writable', true);
     
-    %TODO this is only true if models don't just use 30-day months....
-    days_per_month = mean(month_days(start_month(i):end_month(i)));
-    
-    %If I'm going to do a 9-year running average CENTERED at 1901 and 1999,
-    %then I need to start at 1897 and end at 2003.
-    %It seems that the observational data doesn't start before 1901!
-    time_range = ['T/%28', month0, '%20', num2str(start_year), '%29%28',...
-        month1, '%20', num2str(end_year), '%29RANGE/'];
-    seasonal_average = ['T/%28', month0, '-', month1,'%29/seasonalAverage/',...
-        'T/12/STEP/dods'];
-    url_end_h = [space_range_average, time_range, seasonal_average];
-    url_end_piC = [space_range_average, seasonal_average];
-    
-    %Observations
-    if(get_observations)
-        %TODO these are old observations file and attribute names
+    if(strcmp(variable, 'pr'))
         observations_url = [...
             'http://iridl.ldeo.columbia.edu/SOURCES/',...
-            '.WCRP/.GCOS/.GPCC/.FDP/.version7/.1p0/.prcp/',...
+            '.WCRP/.GCOS/.GPCC/.FDP/.version2018/.1p0/.prcp/',...
             'X/-20/40/RANGE/',...
             'Y/',num2str(s_lat), '/', num2str(n_lat), '/RANGE/',...
             '%7B/Y/cosd/%7D/',...
-            '%5BX/Y%5D/weighted-average/', time_range, seasonal_average];
+            '%5BX/Y%5D/weighted-average/', seasonal_average]; %removed time_range!
+        obs_units = ncreadatt(observations_url, 'T', 'units');
+        
+        prcp_unstandardized = ncread(observations_url, 'prcp');
+        ObsFile.var = rainfall_to_days(prcp_unstandardized', days_per_month);
+        ObsFile.src = 'GPCC version18';
 
         observations_url_CRU = [...
             'http://kage.ldeo.columbia.edu:81/expert/home/.datasets/.CRU3.25/.cru3p25.nc/.pre/',...
             'X/-20/40/RANGE/',...
             'Y/',num2str(s_lat), '/', num2str(n_lat), '/RANGE',...
             '%7B/Y/cosd/%7D/',...
-            '%5BX/Y%5D/weighted-average/', time_range, seasonal_average];
+            '%5BX/Y%5D/weighted-average/', seasonal_average]; %removed time_range!
+        
+        CRU_obs = matfile(['data/', variable,'/CRU_data.mat'], 'Writable', true);
+        CRU_obs.var = rainfall_to_days(ncread(observations_url_CRU, 'pre')', days_per_month);
+        CRU_obs.T = adjust_time(ncread(observations_url_CRU, 'T'), get_ref_year(ncreadatt(observations_url_CRU, 'T', 'units')));
+        CRU_obs.src = 'CRU';
 
-        % TODO fix this to a weighted average and put back in
-        %SST_forced_url = 'http://iridl.ldeo.columbia.edu/SOURCES/.IRI/.FD/.NSIPP-1/.History/.ensemble/.monthly/.prcp/Y/cosd/mul/X/-20/40/RANGE/Y/12/18/RANGE/%5BX/Y%5Daverage/T/%28Jul%201950%29/%28Sep%202000%29/RANGE/T/%28Jul-Sep%29/seasonalAverage/T/12/STEP/dods';
-
-        prcp_unstandardized = ncread(observations_url, 'prcp'); 
-        ref_T_months = ncread(observations_url,'T');
-        ref_T_years = adjust_time(ref_T_months, 1960);
-        L = length(ref_T_years);
-
-        %TODO: update make observations file name function
-        ObsFile = matfile(make_observations_file_name(month0, month1),'Writable', true);
-        ObsFile.prcp(1,1:L) = rainfall_to_days(prcp_unstandardized', days_per_month);
-        %To avoid redundancy, we only save the years for the observations, and
-        %check that the years for the model outputs would have matched.
-        ObsFile.T(1,1:L) = ref_T_years';
-
-        CRU_obs = matfile(['data/', month0, '-',month1,'/CRU_data.mat'], 'Writable', true);
-        CRU_obs.prcp(1,1:L) = rainfall_to_days(ncread(observations_url_CRU, 'pre')', days_per_month);
+        % Uses weighting as done by Alessandra Giannini in her 2003 paper
+        %{
+        Ale_2003 = [...
+            'http://iridl.ldeo.columbia.edu/SOURCES/',...
+            '.IRI/.FD/.NSIPP-1/.History/.ensemble/.monthly/.prcp/',...
+            'Y/cosd/mul/',...
+            'X/-20/40/RANGE/',...
+            'Y/12/18/RANGE/',...
+            '%5BX/Y%5Daverage/', seasonal_average];
+        %}
+    else
+        %TODO: fill in TS!!!!!
     end
-    
+    ref_T_months = ncread(observations_url,'T');
+    obs_T_years = adjust_time(ref_T_months, get_ref_year(obs_units));
+    ObsFile.T = obs_T_years';
+end
+
+%Historical Simulations
+if(get_historical)
     for j = 1:length(scenarios)
         clear model
         scenario = scenarios{j};
@@ -139,7 +153,7 @@ for i = 1:length(start_month)
         fprintf("Accessing scenario %s\n", scenario);
         fprintf("Opening file %s_models.txt\n", scenario);
         models = fopen([scenario, '_models.txt']);
-        
+
         file_name = fgetl(models);
         next_line=1;
         while ischar(file_name)
