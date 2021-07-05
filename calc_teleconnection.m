e589d9960ab2c76f7dd712bca76428f640c7e973
rebeccaherman1@berkeley.edu <rebeccaherman1@berkeley.edu>
2021-07-05 15:51:16 -0400

    revamped calc_teleconnections so it's giving better info, is more customizable, and presents the info in a more readable way.

--- calc_teleconnection.m
+++ calc_teleconnection.m
@@ -1,33 +1,63 @@
-NARI = load('data/ts/observations.mat');
-NARI = NARI.var(:,:,3);
-amip_piF = load('Analysis/pr/amip-piF_1901-2014_N500.mat');
+end_yr = 2003;
+
 obs = load('data/pr/observations.mat');
-obs = obs.var(1901<=obs.T & obs.T <= 2014);
-amip_hist = load('Analysis/pr/amip-hist_1901-2014_N500.mat');
+obs = obs.var(1901<=obs.T & obs.T <= end_yr);
+NARI = load('data/ts/MMM_data/observations.mat');
+NARI = NARI.var(:,1:(end_yr - 1901 + 1),3);
 
-NARI_lo = smooth(NARI, 19);
+C = cov(NARI', obs');
+t = C(1,2)/C(1,1);
+fprintf('For obs: %f\n\n', t)
 
-corr(NARI', obs')
-corr(NARI', amip_hist.MMM.MMM')
-
-r = corr(NARI', amip_piF.MMM.MMM')
-rs = corr(NARI', amip_piF.historical_bootstrapped.b_means');
-[r_lo, r_hi] = confidence_interval(rs,2);
-r_hat = mean(rs)
-r_mid = mean([r_lo, r_hi])
-r_2sig_hat = 2*std(rs)
-r_2sig_mid = mean([r_mid-r_lo, r_hi-r_mid])
-
-C = cov(NARI', amip_piF.MMM.MMM');
-t = C(1,2)/C(1,1)
-ts = nan(500,1);
-for i = 1:500
-    bm = amip_piF.historical_bootstrapped.b_means(i,:);
-    C = cov(NARI', bm');
-    ts(i) = C(1,2)/C(1,1);
+%sims = {'amip-piF', 'amip-hist', 'cmip6_h', 'cmip6_a', 'cmip6_n', 'cmip6_g'};
+sims = {'h', 'a', 'n', 'g'};
+for s = 1:length(sims)
+    sm = sims{s};
+    S = load(['Analysis/pr/',sm,'_1901-',num2str(end_yr),'_N500.mat']);
+    if(~contains(sm, 'amip'))
+        NARI = load(['Analysis/ts/',sm,'_1901-',num2str(end_yr),'_N500.mat']);
+        NARI = NARI.MMM.MMM(:,:,3);
+    end
+    %{
+    r = corr(NARI', S.MMM.MMM')
+    rs = corr(NARI', S.historical_bootstrapped.b_means');
+    [r_lo, r_hi] = confidence_interval(rs,2);
+    r_hat = mean(rs)
+    r_mid = mean([r_lo, r_hi])
+    r_2sig_hat = 2*std(rs)
+    r_2sig_mid = mean([r_mid-r_lo, r_hi-r_mid])
+    %}
+    C = cov(NARI', S.MMM.MMM');
+    t = C(1,2)/C(1,1);
+    ts = nan(500,1);
+    for i = 1:500
+        bm = S.historical_bootstrapped.b_means(i,:);
+        C = cov(NARI', bm');
+        ts(i) = C(1,2)/C(1,1);
+    end
+    [t_lo, t_hi] = confidence_interval(ts);
+    t_hat = mean(ts);
+    t_mid = mean([t_lo, t_hi]);
+    t_2sig_hat = 2*std(ts);
+    t_2sig_med = mean([t_mid - t_lo, t_hi - t_mid]);
+    fprintf('For %s:\n %f, [%f, %f]\n %f +/- %f\n %f +/- %f\n\n', sm, t, t_lo, t_hi, t_hat, t_2sig_hat, t_mid, t_2sig_med)
 end
-[t_lo, t_hi] = confidence_interval(ts);
-t_hat = mean(ts)
-t_mid = mean([t_lo, t_hi])
-t_2sig_hat = 2*std(ts)
-t_2sig_med = mean([t_mid - t_lo, t_hi - t_mid])
+
+indiv_pr = load('data/pr/cmip6_piC_all.mat');
+indiv_ts = load('data/ts/MMM_data/cmip6_piC_all.mat');
+prm = strcat(indiv_pr.model(:,1), indiv_pr.model(:,2), indiv_pr.model(:,3));
+tsm = strcat(indiv_ts.model(:,1), indiv_ts.model(:,2), indiv_ts.model(:,3));
+pr_inc = contains(prm, tsm); [prM, prI] = sort(prm(pr_inc));
+ts_inc = contains(tsm, prm); [tsM, tsI] = sort(tsm(ts_inc));
+pr_t = indiv_pr.time(pr_inc,:); ts_t = indiv_ts.time(ts_inc,:); 
+dimlim = min(size(pr_t,2), size(ts_t,2));
+pr_t = pr_t(prI,1:dimlim); ts_t = ts_t(tsI,1:dimlim);
+pr = indiv_pr.runs(pr_inc,:); pr = pr(prI,1:dimlim); pr(pr==0)=nan; 
+ts = indiv_ts.runs(ts_inc,:,3); ts = ts(tsI,1:dimlim); ts(ts==0)=nan; 
+t_indiv = nan(size(pr,1),1);
+for i = 1:size(pr, 1)
+    C = nancov(ts(i,:)',pr(i,:)');
+    t_indiv(i) = C(1,2)/C(1,1);
+end
+[lo, hi] = confidence_interval(t_indiv);
+fprintf('For piC simulations, %f [%f, %f]\n\n', mean(t_indiv), lo, hi)
