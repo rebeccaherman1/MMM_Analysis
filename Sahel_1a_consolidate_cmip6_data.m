32127f142fefd6d0a9ab054656377d1e2efcc6c6
Rebecca Jean Herman <rebeccaherman1@berkeley.edu>
2021-07-08 14:13:24 -0400

    major motifications to Sahel_1a. made it flexible to havind additional variables and dimensions. restricted concatenation to 'ts' because of calculating 'p1'. Must rethink 'Ocean' LOCATION later. Downloaded and compiled ta and zg over the whole atmosphere.

--- Sahel_1a_consolidate_cmip6_data.m
+++ Sahel_1a_consolidate_cmip6_data.m
@@ -1,96 +1,116 @@
 %Only use on PREPROCESSED data which was downloaded from the cloud via my
 %jupyter script. For data I downloaded in full, see matlab code
 %Sahel_1_save_data_amip.
-
+%variables that are included in some models but not others can be found in SKIPPED_VARS
 %TODO check why CSIRO files give segfaults.
 
 clear
-variable = 'ta';
+variable = 'zg';
 location = 'Sahel';
 
 scenarios = {'historical'};%, 'hist-aer'};%, 'hist-nat', 'hist-GHG'};%'amip-hist', 'piControl'};
 short_names = {'cmip6_h', 'cmip6_a', 'cmip6_n', 'cmip6_g'};%'amip-hist', 'cmip6_piC'};
+skipped_vars = cell(1,6);
 
 for i = 1:length(scenarios)
-    clear model runs time
+    %create folder and filename where compiled data will be saved
     fldr_name = ['data/', variable];
-    if strcmp(variable, 'zg') || strcmp(variable, 'ta')
-	fldr_name = [fldr_name, '100'];
-    end
     if ~exist(fldr_name, 'dir')
 	mkdir(fldr_name);
     end
     model_file_name = [fldr_name, '/', short_names{i}, '_all.mat']
+    %get list of files to compile
     folder = ['~/netcdf/cmip6/preprocessed/', scenarios{i}];
     files = split(ls(folder));
     files = files(contains(files, [variable, '_']));
-    next_line = 1;
     for file = files'
-        names = make_model_and_p_name(file, variable);
-        model(next_line, 1:3) = names;
         fopen_name = [folder, '/', file{:}];
         %ncdisp(fopen_name)
         INFO = ncinfo(fopen_name);
-	if strcmp(variable, 'ts') && ~any(contains({INFO.Variables.Name}, {'SA'})) || contains(fopen_name, 'CSIRO')
-	    fprintf("skipping file %s\n", file{:})
-	    continue;
-        else
+	vars = {INFO.Variables.Name};
+%	if strcmp(variable, 'ts') && ~any(contains(vars, {'SA'})) || contains(fopen_name, 'CSIRO')
+%	    fprintf("skipping file %s\n", file{:})
+%	    continue;
+%        else
 	    fprintf("processing file %s\n", file{:})
-	end
-        if any(contains({INFO.Variables.Name}, {'time'}))
-            Time = ncread(fopen_name, 'time');
-        elseif any(contains({INFO.Variables.Name}, {'year'}))
-            Time = ncread(fopen_name, 'year');
+%	end
+	%downloads all available data into a cell array of varying length
+	Dims = {INFO.Variables.Dimensions};
+	D = cell(length(vars), 1);
+	for v = 1:length(vars)
+	    d = ncread(fopen_name, vars{v});
+	    Lengths = [Dims{v}.Length];
+	    dims = {Dims{v}.Name};
+	    %make sure the first dimension has length 1 and that time is the second dimension.
+	    D{v} = permute(d, [length(Lengths)+1, find(strcmp(dims, 'time') | strcmp(dims, 'year')),...
+		                   find(~(strcmp(dims, 'time') | strcmp(dims, 'year')))]);
         end
-        if(strcmp(location, 'Sahel'))
-	    fprintf('fopen_name = %s, variable = %s\n', fopen_name, variable)
-	    V = split(variable, ["_"]);
-	    if(any(contains(V, 'conv')))
-		V = '__xarray_dataarray_variable__';
-	    else
-		V = V{1};
+	%rename 'year' to 'time' if needed
+	if(any(contains(vars, 'year')))
+	    vars{strcmp(vars, 'year')} = 'time';
+        end
+	Time = D{strcmp(vars, 'time')};
+	%if the variable is ts, combine the basins into one array called VARIABLE.
+        if(strcmp(variable, 'ts'))
+	    %Add p1 if missing.
+	    if(~any(contains(vars, 'p1')))
+		p1 = D{strcmp(vars, 'NA')} + D{strcmp(vars, 'GT')};
+                D = [D; {p1}];
+	        vars = [vars, {'p1'}];
 	    end
-	    pr = ncread(fopen_name, V);
-	    s3=1;
-        else
-            NA = ncread(fopen_name, 'NA');
-            GT = ncread(fopen_name, 'GT');
-            NARI = ncread(fopen_name, 'NARI');
-            p1 = NA + GT;
-            SA = ncread(fopen_name, 'SA');
-            md = ncread(fopen_name, 'md');
-            pr = cat(3, NA', GT', NARI', SA', md');
-            s3=size(pr, 3);
-            indices = permute({'NA', 'GT', 'NARI', 'SA', 'md'},[1,3,2]);
+	    all_basins = ~(strcmp(vars, 'time'));
+	    pr = cat(3, D{all_basins});
+	    indices = vars(all_basins);
+	    D = {pr; Time; indices};
+	    vars = {variable, 'time', 'indices'};
         end
-        if contains(model_file_name, 'piC')
+	%Find the variable of interest
+	if(contains(variable, 'conv'))
+	    V = '__xarray_dataarray_variable__';
+        else
+	    V = variable;
+        end
+        %restrict time to the years of interest.
+	if contains(model_file_name, 'piC')
             l = length(Time);
         elseif Time(1)<=1901 && Time(end)>=2014
             T_x = (Time >= 1901) & (Time <= 2014);
-            Time = Time(T_x);
-            if(ndims(pr)>2)
-		pr = pr(:,T_x,:);
-	    elseif (size(pr,2)==1)
-	        pr = pr(T_x)';
-	    else
-		pr = pr(end,T_x); %picking plev = 100 hPa for those files where I took too much. 
-	    end
-            l = size(pr,2);
-            if(length(Time)~=l)
+            D{strcmp(vars, 'time')} = Time(T_x);
+	    Dv = D{strcmp(vars, V)};
+	    D{strcmp(vars, V)} = Dv(:,T_x,:);
+	    l = size(D{strcmp(vars, V)},2);
+            if(length(D{strcmp(vars, 'time')})~=l)
                 fprintf('pr contains fill values\n');
             end
         else
-            continue
+            fprintf('does not cover desired time period\n');
+	    continue
         end
-        runs(next_line,1:l,1:s3) = pr; 
-        time(next_line,1:l) = Time;
-        next_line=next_line+1;
-        if(strcmp(location, 'Ocean'))
-            save(model_file_name, 'model', 'runs', 'time', 'indices')
+	%add institution, model, run to the cell array D
+	D{length(vars)+1} = make_model_and_p_name(file, variable);
+	vars{length(vars)+1} = 'model';
+	%concatenate with previous files
+	if(~exist('D_tot', 'var'))
+	    D_tot = D;
+	    vars_tot = vars;
         else
-            save(model_file_name,'model','runs', 'time');
+	    for di = 1:length(vars)
+		dti = strcmp(vars_tot, vars{di});
+		if(any(dti))
+		    D_tot{dti} = cat(1, D_tot{dti}, D{di});
+		else
+		    fprintf('missing variable %s\n', vars{di});
+		    M = D{strcmp(vars, 'model')};
+		    skipped_vars = cat(1, skipped_vars, {scenarios{i}, M{:}, vars{di}, D{di}});
+	        end
+	    end
         end
     end
+    %change the name of the variable of interest to 'runs' to make later code easier. 
+    %the name of the variable will be preserved in the MODEL_FILE_NAME
+    vars{strcmp(vars, V)} = 'runs';
+    S = cell2struct(D_tot, vars_tot);
+    save(model_file_name, '-struct', 'S');
 end
 
 %{
