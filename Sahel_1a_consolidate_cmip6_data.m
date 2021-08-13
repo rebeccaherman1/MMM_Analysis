32bc7cb18298595828c1e00c3357b2e2ba5bbe22
Rebecca Jean Herman <rebeccaherman1@berkeley.edu>
2021-08-13 13:37:40 -0400

    Downloaded and compiled ua. hus_bndries will definitely need to be redone

--- Sahel_1a_consolidate_cmip6_data.m
+++ Sahel_1a_consolidate_cmip6_data.m
@@ -5,7 +5,7 @@
 %TODO check why CSIRO files give segfaults.
 
 clear
-variable = 'va';
+variable = 'ua';
 %location = 'Sahel'; Not currently used. Perhaps use ~strcmp(location, Sahel) for the ocean basins instead of strcmp(variable, ts)
 start_month = 7;
 end_month = 9;
